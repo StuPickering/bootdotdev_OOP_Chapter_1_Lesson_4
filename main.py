7fac011f7842952ef7541239b20b069f23dc5f1e
Stu_Pickering <magus123456789@hotmail.com>
2026-06-03 08:46:10 +0100

    G: Refactored main.py to only have 1 return and correctly measure dps

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 def fight_soldiers(soldier_one, soldier_two):
-    soldier_one_dps = soldier_one["damage"] * soldier_one["attacks_per_second"]
-    soldier_two_dps = soldier_two["damage"] * soldier_two["attacks_per_second"]
+
+    soldier_one_dps = soldier_dps(soldier_one)
+    soldier_two_dps = soldier_dps(soldier_two)
+
     if soldier_one_dps > soldier_two_dps:
-        return "soldier 1 wins"
-    if soldier_two_dps > soldier_one_dps:
-        return "soldier 2 wins"
-    return "both soldiers die"
+        winner= "soldier 1 wins"
+    
+    elif soldier_two_dps > soldier_one_dps:
+        winner= "soldier 2 wins"
+    
+    else:
+        winner= "both soldiers die"
+
+    return winner, soldier_one_dps, soldier_two_dps
+
+def soldier_dps(soldier_dict):
+    return soldier_dict["damage"] * soldier_dict["attacks_per_second"]
